1eddfafc7931ada97f8d2a1b396aa41fba7bb014
kn541 <kn541dev@gmail.com>
2026-04-20 15:28:58 +0900

    fix: deepmerge import를 @mui/utils → @mui/material/utils로 변경 (패키지 추가 없이 해결)

--- src/components/theme/index.tsx
+++ src/components/theme/index.tsx
@@ -4,8 +4,8 @@
 import { useMemo } from 'react'
 
 // MUI Imports
-import { deepmerge } from '@mui/utils'
 import { ThemeProvider, lighten, darken, createTheme } from '@mui/material/styles'
+import { deepmerge } from '@mui/material/utils'
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter'
 import CssBaseline from '@mui/material/CssBaseline'
 import type {} from '@mui/material/themeCssVarsAugmentation' //! Do not remove this import otherwise you will get type errors while making a production build
